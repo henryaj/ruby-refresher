fbb85ee3e5ac1432aec0d974328539f758da741b
Henry Stanley <henry@henrystanley.com>
2014-09-26 15:44:20 +0100

    Kind of a weird solution for this one... never head of ceil before

--- questions_spec.rb
+++ questions_spec.rb
@@ -50,17 +50,19 @@ describe 'the Friday test :)' do
     expect(n).to eq [1, 2, 3, 4, 5]
   end
 
-  fit 'array_sort_by_last_letter_of_word' do
+  specify 'array_sort_by_last_letter_of_word' do
     n = array_sort_by_last_letter_of_word ['sky', 'puma', 'maker']
     expect(n).to eq ['puma', 'maker', 'sky']
   end
 
-  specify 'get_first_half_of_string' do
+  fit 'get_first_half_of_string' do
     a = get_first_half_of_string 'banana'
     b = get_first_half_of_string 'apple'
+    c = get_first_half_of_string 'dogglezz'
 
     expect(a).to eq 'ban'
     expect(b).to eq 'app'
+    expect(c).to eq 'dogg'
   end
 
   specify 'make_numbers_negative' do
