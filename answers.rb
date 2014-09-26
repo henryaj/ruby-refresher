ed81d4dfa8ddafb99953dc67e663f02af7300112
Henry Stanley <henry@henrystanley.com>
2014-09-26 14:57:05 +0100

    Second test passing

--- answers.rb
+++ answers.rb
@@ -1,3 +1,8 @@
 def select_elements_starting_with_a(array)
   array.select { |item| item.start_with? 'a' }
 end
+
+def select_elements_starting_with_vowel(array)
+  array.select { |item| item.start_with? 'a','e','i','o','u' }
+end
+
